19eedf9edf5d625cea424da3d71fe2df11db41b7
Jackson Sanchez <angeljackson0933@gmail.com>
2022-04-05 22:49:02 -0500

    rest models

--- premios_platzi_app/polls/tests.py
+++ premios_platzi_app/polls/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+# Models or views
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_questions(self):
+        """ was_published_recently return False
+        for question whose pub_date in the future """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(
+            question_text="Quien es el mejor CD de Platzi?", pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
--- premios_platzi_app/users/models.py
+++ premios_platzi_app/users/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.db import timezone
 # Create your models here.
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email
+        }
